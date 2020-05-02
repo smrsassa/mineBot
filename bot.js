fa02ee963164ce9eb02176ab34b27e4a0d6f7423
SMR-sassa <samrufino13@gmail.com>
2020-05-02 15:36:26 -0300

    enviar msg de bem vindo

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const jimp = require('jimp');
 const client = new Discord.Client();
 const config = require("./config.json");
 
@@ -16,7 +17,34 @@ client.on("guildDelete", guild => {
   console.log(`O bot foi removido do servidor: ${guilds.name} (id: ${guilds.id})`);
   client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
+/* Enviar imagem de bem vindo
+client.on("guildMemberAdd", async member => {
 
+  let canal = client.channels.get("705225611878203505")
+  let fonte = await jimp.loadFont(jimp.FONT_SANS_32_BLACK);
+  let masc = await jimp.read('img/mascara.png');
+  let fundo = await jimp.read('img/fundo.png');
+
+  jimp.read(member.user.displayAvatarURL)
+    .then(avatar => {
+
+      avatar.resize(130, 130);
+      masc.resize(130, 130);
+      avatar.mask(masc);
+
+      fundo.print(fonte, 180, 175, member.user.username);
+
+      fundo.composite(avatar, 30, 30).write('users_img/bemvindo.png');
+
+      canal.send(``, { files: ["bemvindo.png"] });
+
+    })
+    .catch(err => {
+      console.log('ERRO: erro ao carregar a imagem (jimp.js)');
+    });
+
+});
+*/
 client.on('message', message => {
   if (message.content === '!casa') {
     message.channel.send('X: 546 Y: 64 Z: 123 | Comando: /tp 546 64 123');
